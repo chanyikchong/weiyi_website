5b1be096a03e74a3982c4ed2d5dcd49877f436d4
Yichong Chen <51711007+chanyikchong@users.noreply.github.com>
2020-10-16 22:24:30 +0100

    remove wrong slide window

--- func.js
+++ func.js
@@ -13,44 +13,6 @@ function music(){
     }
 }
 
-function slide() {
-    function changepic(){
-        
-        img.src = pic[pic_idx];
-        // img.width = pic_wh[pic_idx][0];
-        // img.height = pic_wh[pic_idx][1];
-        pic_idx ++;
-        if (pic_idx>=pic.length){
-            pic_idx = 0;
-        }
-    }
 
-    var img = document.getElementById("wypic");
-    
-    var pic = [
-        './img/wy1.jpg',
-        './img/wy2.jpg',
-        './img/wy3.jpg',
-        './img/wy4.jpg',
-        './img/wy5.jpg',
-        './img/wy6.jpg',
-        './img/wy7.jpg',
-        './img/wy8.jpg',
-        './img/wy9.jpg',
-        './img/wy10.jpg'
-    ];
-    
-    var slide_start = setInterval(changepic,5000);
-    img.onmouseover = function () {
-        clearInterval(slide_start);
-    }
-    img.onmouseout = function () {
-        slide_start = setInterval(changepic,5000);
-    }
-
-}
 var is_play=true;
-// var pic_idx = 1;
-// slide()
 window.onload = music();
-console.log(document.getElementById("second_container").offsetWidth) 
