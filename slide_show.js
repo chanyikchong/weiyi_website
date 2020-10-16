88605e666823a3311dc9d17c6fc1f2c2b784503c
Yichong Chen <51711007+chanyikchong@users.noreply.github.com>
2020-10-16 11:12:57 +0100

    flex

--- slide_show.js
+++ slide_show.js
@@ -1,18 +1,22 @@
 $(function () {
     // 实现自动播放
     var p=document.getElementsByClassName('img-g')[0];
+    var img=p.getElementsByTagName("img")
     var button=document.querySelectorAll('.button-g span')
     // 设置3秒播放
     window.timer=setInterval(move,5000);
     // 轮播设置
+    var move_x = img[0].clientWidth
     function move(){
         // bannerimage的宽度乘以图片的个数
+
         if(parseInt(p.style.left)>-4200){
             // 和bannerimage的宽度保持一致即可：700
-            p.style.left=parseInt(p.style.left)-750+'px'
+            move_x = img[0].clientWidth
+            p.style.left=parseInt(p.style.left)-move_x+'px'
             p.style.transition='left 1s';
-            tog(-Math.round(parseInt(p.style.left)/750))
-            if(parseInt(p.style.left)<=-4500){
+            tog(-Math.round(parseInt(p.style.left)/move_x))
+            if(parseInt(p.style.left)<=-move_x*img.length){
                 setTimeout(function(){
                     tog(0)
                     p.style.left='0px'
@@ -29,7 +33,7 @@ $(function () {
     for(var i=0;i<button.length;i++){
         // button[i].style.backgroundColor='#eee';
         button[i].onclick=function(){
-            p.style.left=-750*this.getAttribute('data-index')+'px'
+            p.style.left=-move_x*this.getAttribute('data-index')+'px'
             tog(this.getAttribute('data-index'))
             clearInterval(window.timer)
             window.timer=setInterval(move,5000);
@@ -37,6 +41,8 @@ $(function () {
     }
     // 设置小圆点
     function tog(index){
+        move_x = img[0].clientWidth
+        console.log(move_x)
         if(index>5){
             tog(0);
             return;
